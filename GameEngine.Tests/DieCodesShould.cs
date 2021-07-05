3e5f139b929453f831a602a13e40f5796663102c
Matt DeKrey <mattdekrey@gmail.com>
2021-07-04 21:41:59 -0500

    Add initial die code class

---
+++ GameEngine.Tests/DieCodesShould.cs
@@ -0,0 +1,28 @@
+using System;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class DieCodesShould
+    {
+        [Theory]
+        [InlineData("1d4+2d6", "2d6 + d4")]
+        [InlineData("1d4+2d6+7", "2d6 + d4 + 7")]
+        [InlineData("-1d4+2d6+7", "2d6 - d4 + 7")]
+        [InlineData("-d4-2d6+7", "-d4 - 2d6 + 7")]
+        [InlineData("7", "+7")]
+        [InlineData("1d6", "d6")]
+        [InlineData("d6", "d6")]
+        [InlineData("-d12 + d6", "d6 - d12")]
+        [InlineData("-d12 + d12", "+0")]
+        [InlineData("-d12 + d12 - 2", "-2")]
+        [InlineData("-d12 + d12 + 1d6 - 2d6 + d6 + 6 - 4", "+2")]
+        [InlineData("-d12 + d12 + 1d6 + 6 - 2d6 + d6 - 4", "+2")]
+        [InlineData("-d12 +d8+ d12 + 1d6 + 6 - 2d6 + d6 - 4", "d8 + 2")]
+        public void SuccessfullyParse(string original, string normalized)
+        {
+            Assert.True(DieCodes.TryParse(original, out var dieCodes));
+            Assert.Equal(normalized, dieCodes.ToString());
+        }
+    }
+}
---
+++ GameEngine/Dice.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace GameEngine
+{
+    public record DieCode(
+        int DieCount,
+        int Sides
+    )
+    {
+        public override string ToString() =>
+            $"{(DieCount == 1 ? DieCount : "")}d{Sides}";
+
+        private static Regex dieCodeRegex = new Regex("^(?<sign>[+-])?(?<count>([1-9][0-9]*)?)d(?<sides>[1-9][0-9]*)$");
+        public static bool TryParse(string input, [NotNullWhen(true)] out DieCode? dieCode)
+        {
+            switch (dieCodeRegex.Match(input.Replace(" ", "")))
+            {
+                case { Success: true, Groups: var groups }:
+                    dieCode = new DieCode(
+                        (groups["sign"].Value == "-" ? -1 : 1) *
+                        (int.TryParse(groups["count"].Value, out var count) ? count : 1), 
+                        int.Parse(groups["sides"].Value)
+                    );
+                    return true;
+                case { Success: false }:
+                default:
+                    dieCode = null;
+                    return false;
+            };
+        }
+    }
+
+    public record DieCodes(
+        ImmutableList<DieCode> Entries,
+        int Modifier
+    )
+    {
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+
+            foreach (var entry in Entries)
+                sb.Append(WithModifier(entry.DieCount, true)).Append(entry.DieCount > 1 || entry.DieCount < -1 ? Math.Abs(entry.DieCount) : "").Append("d").Append(entry.Sides);
+            if (Modifier != 0 || sb.Length == 0)
+                sb.Append(WithModifier(Modifier, false)).Append(Math.Abs(Modifier));
+            return sb.ToString();
+
+            string WithModifier(int number, bool optional)
+            {
+                return (sb.Length, number) switch
+                {
+                    (> 0, < 0) => " - ",
+                    (> 0, > 0) => " + ",
+                    (_, < 0) => "-",
+                    (_, >= 0) when !optional => "+",
+                    _ => "",
+                };
+            }
+        }
+
+
+        private static Regex dieCodesRegex = new Regex("^((?<dieCode>([+-]? ?([1-9][0-9]*)?)d[1-9][0-9]*)|(?<modifier>[+-]?[1-9][0-9]*))(?<extra>([+-] ?([1-9][0-9]*)?(d[1-9][0-9]*)? ?)+?)?$");
+
+        public static DieCodes Empty { get; } = new DieCodes(ImmutableList.Create<DieCode>(), 0);
+
+        public static bool TryParse(string input, [NotNullWhen(true)] out DieCodes? dieCodes)
+        {
+            input = input.Replace(" ", "");
+            switch (dieCodesRegex.Match(input))
+            {
+                case { Success: true, Groups: var groups }:
+                    var dc = !groups["dieCode"].Success ? Empty : (DieCode.TryParse(groups["dieCode"].Value, out var dieCode) ? dieCode : throw new InvalidOperationException());
+                    var extra = !groups["extra"].Success ? Empty : (TryParse(groups["extra"].Value, out var diceCode) ? diceCode : throw new InvalidOperationException());
+                    var mod = !groups["modifier"].Success ? Empty : (int.TryParse(groups["modifier"].Value, out var modifier) ? modifier : throw new InvalidOperationException());
+                    dieCodes = dc + extra + mod;
+                    return true;
+                case { Success: false } when int.TryParse(input, out var result):
+                    dieCodes = result;
+                    return true;
+                case { Success: false }:
+                default:
+                    dieCodes = null;
+                    return false;
+            };
+        }
+
+        public static DieCodes operator +(DieCodes lhs, DieCodes rhs) =>
+            new DieCodes(
+                (from entry in lhs.Entries.Concat(rhs.Entries)
+                 group entry.DieCount by entry.Sides into dieCode
+                 let dieCount = dieCode.Sum()
+                 where dieCount != 0
+                 orderby dieCount descending
+                 select new DieCode(dieCount, dieCode.Key)
+                ).ToImmutableList(), lhs.Modifier + rhs.Modifier);
+        public static implicit operator DieCodes(DieCode dieCode) =>
+            new DieCodes(ImmutableList.Create(dieCode), 0);
+        public static implicit operator DieCodes(int modifier) =>
+            Empty with { Modifier = modifier };
+    }
+}
