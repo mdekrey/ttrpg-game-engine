021261885f320d7247fe62c9098f1488d2ac2f4c
Matt DeKrey <mattdekrey@gmail.com>
2021-07-08 06:33:17 -0500

    Start working on encounter expectations

---
+++ GameEngine.Tests/EncounterExpectationsShould.cs
@@ -0,0 +1,60 @@
+﻿using GameEngine.Dice;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace GameEngine.Tests
+{
+    public class EncounterExpectationsShould
+    {
+        [Fact]
+        public void MeetAverageEncounterLengthForSingleMonster()
+        {
+            const int level = 1;
+            const double chanceToHit = 0.5;
+            var pcBonusDamagePerRound = DieCodes.Parse(CombatExpectations.limitedDamage[level / 3].High).Mean() * chanceToHit;
+            var pcStandardDamagePerRound = DieCodes.Parse(CombatExpectations.standardDamage[level / 3].High).Mean() * chanceToHit;
+
+            var damage = GenerateDamagePerAction(new[] { (1, pcBonusDamagePerRound) }, pcStandardDamagePerRound);
+            
+            //based on Goblin Warrior
+            const int monsterHp = 29;
+            var damagePerRound = 
+                (DieCodes.Parse(CombatExpectations.standardDamage[level / 3].High).Mean() * 0.5 
+                + DieCodes.Parse(CombatExpectations.standardDamage[level / 3].Medium).Mean() * 0.5)
+                * chanceToHit;
+
+            var healthRemainingPerRound = damage.Scan((double)monsterHp, (hp, damage) => hp - damage).TakeWhile(hp => hp > 0).ToArray();
+            Assert.Equal(5, healthRemainingPerRound.Length);
+        }
+
+        private IEnumerable<double> GenerateDamagePerAction((int times, double damage)[] limitedDamage, params double[] atWillDamage)
+        {
+            foreach (var entry in limitedDamage.OrderByDescending(v => v.damage))
+            {
+                for (var i = 0; i < entry.times; i++)
+                    yield return entry.damage;
+            }
+            while(true)
+            {
+                foreach (var entry in atWillDamage)
+                    yield return entry;
+            }
+        }
+    }
+
+    public static class ScanExtensions
+    {
+        public static IEnumerable<TResult> Scan<TInput, TResult>(this IEnumerable<TInput> inputs, TResult seed, Func<TResult, TInput, TResult> reducer)
+        {
+            TResult current = seed;
+            foreach (var next in inputs)
+            {
+                current = reducer(current, next);
+                yield return current;
+            }
+        }
+    }
+}
--- GameEngine/Rules/CombatExpectations.cs
+++ GameEngine/Rules/CombatExpectations.cs
@@ -20,6 +20,33 @@ namespace GameEngine
         public const double averagePrimaryWeaponDamage = 4.5;
         public const double averageSecondaryWeaponDamage = 3.5;
 
+        public static readonly IReadOnlyList<(string Low, string Medium, string High)> standardDamage = new[]
+        {
+            ( Low: "1d6 + 3", Medium: "1d10 + 3", High: "2d6 + 3" ),
+            ( Low: "1d6 + 4", Medium: "1d10 + 4", High: "2d8 + 4" ),
+            ( Low: "1d8 + 5", Medium: "2d6 + 5", High: "2d8 + 5" ),
+            ( Low: "1d8 + 5", Medium: "2d6 + 5", High: "3d6 + 5" ),
+            ( Low: "1d10 + 6", Medium: "2d8 + 6", High: "3d6 + 6" ),
+            ( Low: "1d10 + 7", Medium: "2d8 + 7", High: "3d8 + 7" ),
+            ( Low: "2d6 + 7", Medium: "3d6 + 8", High: "3d8 + 7" ),
+            ( Low: "2d6 + 8", Medium: "3d6 + 8", High: "4d6 + 8" ),
+            ( Low: "2d8 + 9", Medium: "3d8 + 9", High: "4d6 + 9" ),
+            ( Low: "2d8 + 10", Medium: "3d8 + 10", High: "4d8 + 10" ),
+        };
+        public static readonly IReadOnlyList<(string Low, string Medium, string High)> limitedDamage = new[]
+        {
+            ( Low: "3d6 + 3", Medium: "2d10 + 3", High: "3d8 + 3" ),
+            ( Low: "3d6 + 4", Medium: "3d8 + 4", High: "3d10 + 4" ),
+            ( Low: "3d8 + 5", Medium: "3d10 + 5", High: "4d8 + 5" ),
+            ( Low: "3d8 + 5", Medium: "4d8 + 5", High: "4d10 + 5" ),
+            ( Low: "3d10 + 6", Medium: "4d8 + 6", High: "4d10 + 6" ),
+            ( Low: "3d10 + 6", Medium: "4d10 + 7", High: "4d12 + 7" ),
+            ( Low: "4d8 + 7", Medium: "4d10 + 7", High: "4d12 + 7" ),
+            ( Low: "4d8 + 8", Medium: "4d12 + 8", High: "5d10 + 8" ),
+            ( Low: "4d10 + 9", Medium: "5d10 + 9", High: "5d12 + 9" ),
+            ( Low: "4d10 + 9", Medium: "5d10 + 9", High: "5d12 + 9" ),
+        };
+
         public static readonly IReadOnlyList<IReadOnlyList<int>> StandardArrayBy4Levels = new[]
         {
             new[] { 3, 2, 1, 1, 0, 0 },
