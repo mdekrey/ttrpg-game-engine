01aa36d8cbd0b8906a986616140cb1079e85a099
Matt DeKrey <mattdekrey@gmail.com>
2021-07-25 15:06:21 -0500

    Separate basic melee from melee, etc.

--- game-powers-ui/src/components/power/icons/index.tsx
+++ game-powers-ui/src/components/power/icons/index.tsx
@@ -3,3 +3,5 @@ export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as DefenseIcon } from './defense.svg';
 export { ReactComponent as MeleeIcon } from './melee.svg';
 export { ReactComponent as RangedIcon } from './ranged.svg';
+export { ReactComponent as BasicMeleeIcon } from './melee-basic.svg';
+export { ReactComponent as BasicRangedIcon } from './ranged-basic.svg';
