11a2b794ae0e6ef72f1ce03ebab3cfd5daf0b3f6
Matt DeKrey <mattdekrey@gmail.com>
2021-07-31 13:51:00 -0500

    Correct publish-storybook script

--- deployment/publish-storybook.ps1
+++ deployment/publish-storybook.ps1
@@ -5,7 +5,9 @@ $tag = (Get-Date).ToString('yyyy-MM-ddTHH_mm_ss')
 
 docker build . -f .\deployment\Dockerfile.storybook -t dekreydotnet.azurecr.io/game-4e-storybook:$tag
 
-az acr login -name dekreydotnet
+az acr login --name dekreydotnet
 docker push dekreydotnet.azurecr.io/game-4e-storybook:$tag
 
 Pop-Location
+
+kubectl -n game-engine set image deployment game-storybook-deployment web=dekreydotnet.azurecr.io/game-4e-storybook:$tag
