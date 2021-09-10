635f2c94a7b1e3a6e32fc7677d91dc728f597776
Matt DeKrey <mattdekrey@gmail.com>
2021-09-10 08:00:21 -0500

    Add API code generation

---
+++ GameEngine.Web/Api/GenerateClassController.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GameEngine.Web.Api;
+public class GenerateClassController : GenerateClassControllerBase
+{
+    protected override Task<TypeSafeGenerateClassProfileResult> GenerateClassProfileTypeSafe(ClassProfile generateClassProfileBody)
+    {
+        throw new NotImplementedException();
+    }
+}
--- GameEngine.Web/Program.cs
+++ GameEngine.Web/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+builder.Services.AddOpenApiDndClasses<GenerateClassController>();
 
 var app = builder.Build();
 
