3a8348cc30c524a57c4ab32f15c1400daf789c30
Matt DeKrey <mattdekrey@gmail.com>
2021-10-08 07:01:08 -0500

    Prepare download link for React-built SVG

--- game-powers-ui/src/content/rules/character-sheet-container.tsx
+++ game-powers-ui/src/content/rules/character-sheet-container.tsx
@@ -1,18 +1,31 @@
-import charSheetUrl, { ReactComponent } from './character-sheet.svg';
+import { useRef } from 'react';
+import { ReactComponent } from './character-sheet.svg';
 
-export const CharacterSheetContainer = () => (
-	<div className="m-4">
-		<p className="mb-4">
-			<a href={charSheetUrl} target="_blank" className="print:hidden underline text-blue-700" rel="noreferrer">
-				Downloadable
-			</a>
-		</p>
-		<ReactComponent
-			style={{
-				width: '7.5in',
-				height: '10in',
-			}}
-			className="print:m-0 border border-black print:border-0"
-		/>
-	</div>
-);
+export const CharacterSheetContainer = () => {
+	const svgRef = useRef<SVGSVGElement | null>(null);
+	return (
+		<div className="m-4">
+			<p className="mb-4">
+				<button type="button" className="print:hidden underline text-blue-700" onClick={handleLink}>
+					Downloadable
+				</button>
+			</p>
+			<ReactComponent
+				ref={svgRef}
+				style={{
+					width: '7.5in',
+					height: '10in',
+				}}
+				className="print:m-0 border border-black print:border-0"
+			/>
+		</div>
+	);
+	function handleLink() {
+		const s = new XMLSerializer().serializeToString(svgRef.current!);
+		const enc = new TextEncoder();
+		const byteArray = enc.encode(s);
+		const file = new Blob([byteArray], { type: 'image/svg+xml;base64' });
+		const fileURL = URL.createObjectURL(file);
+		window.open(fileURL);
+	}
+};
