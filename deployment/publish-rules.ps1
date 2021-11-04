815b9796c8161daeb74d1fa8dca8d5a215bc2751
Matt DeKrey <matt.dekrey@principlestudios.com>
2021-11-04 06:34:46 -1000

    Add azure account selection

--- deployment/publish-rules.ps1
+++ deployment/publish-rules.ps1
@@ -5,9 +5,12 @@ $tag = (Get-Date).ToString('yyyy-MM-ddTHH_mm_ss')
 
 docker build . -f .\deployment\Dockerfile -t dekreydotnet.azurecr.io/game-4e:$tag
 
+az account set --subscription 2351fc7a-207c-4a7d-8104-d5fe21d7907f
+
 az acr login --name dekreydotnet
 docker push dekreydotnet.azurecr.io/game-4e:$tag
 
 Pop-Location
 
+az aks get-credentials --resource-group DeKreyDotNet -n TinyKubed
 kubectl -n game-engine set image deployment game-deployment web=dekreydotnet.azurecr.io/game-4e:$tag
