9bb8d940e2ca072281d8d3448de9e313a5aadffc
Matt DeKrey <mattdekrey@gmail.com>
2021-12-07 07:28:17 -0600

    Add support for ignoring Superior Cover

--- GameEngine/Generator/Modifiers/IgnoreCoverOrConcealmentFormula.cs
+++ GameEngine/Generator/Modifiers/IgnoreCoverOrConcealmentFormula.cs
@@ -16,6 +16,7 @@ namespace GameEngine.Generator.Modifiers
             if (stage < UpgradeStage.Standard) yield break;
 
             yield return new IgnoreCoverOrConcealment(CoverConcealment.Cover);
+            yield return new IgnoreCoverOrConcealment(CoverConcealment.Cover | CoverConcealment.SuperiorCover);
             yield return new IgnoreCoverOrConcealment(CoverConcealment.Concealment);
             yield return new IgnoreCoverOrConcealment(CoverConcealment.Cover | CoverConcealment.Concealment);
         }
@@ -25,6 +26,7 @@ namespace GameEngine.Generator.Modifiers
         {
             None = 0,
             Cover = 1,
+            SuperiorCover = 2,
             Concealment = 4,
         }
 
@@ -32,14 +34,20 @@ namespace GameEngine.Generator.Modifiers
         {
             public override int GetComplexity(PowerContext powerContext) => 1;
 
-            public override PowerCost GetCost(AttackContext attackContext) => new PowerCost((Kind.HasFlag(CoverConcealment.Cover) ? 0.5 : 0) + (Kind.HasFlag(CoverConcealment.Concealment) ? 0.5 : 0));
+            public override PowerCost GetCost(AttackContext attackContext) => new PowerCost(
+                (Kind.HasFlag(CoverConcealment.Cover) ? 0.5 : 0) 
+                + (Kind.HasFlag(CoverConcealment.SuperiorCover) ? 0.5 : 0) 
+                + (Kind.HasFlag(CoverConcealment.Concealment) ? 0.5 : 0)
+            );
 
             public override IEnumerable<IAttackModifier> GetUpgrades(UpgradeStage stage, AttackContext attackContext)
             {
                 if (!Kind.HasFlag(CoverConcealment.Cover))
                     yield return this with { Kind = Kind | CoverConcealment.Cover };
-                if (!Kind.HasFlag(CoverConcealment.Concealment))
+                if (!Kind.HasFlag(CoverConcealment.Concealment) && !Kind.HasFlag(CoverConcealment.SuperiorCover))
                     yield return this with { Kind = Kind | CoverConcealment.Concealment };
+                if (!Kind.HasFlag(CoverConcealment.Concealment) && Kind.HasFlag(CoverConcealment.Cover) && !Kind.HasFlag(CoverConcealment.SuperiorCover))
+                    yield return this with { Kind = Kind | CoverConcealment.SuperiorCover };
             }
 
             public override AttackInfoMutator? GetAttackInfoMutator(AttackContext attackContext) =>
@@ -47,6 +55,7 @@ namespace GameEngine.Generator.Modifiers
                     Kind switch
                     {
                         CoverConcealment.Cover => "The attack ignores cover, but not superior cover.",
+                        CoverConcealment.Cover | CoverConcealment.SuperiorCover => "The attack ignores cover and superior cover.",
                         CoverConcealment.Concealment => "The attack ignores concealment, but not total concealment.",
                         CoverConcealment.Cover | CoverConcealment.Concealment => "The attack ignores cover and concealment, but not superior cover or total concealment.",
 
