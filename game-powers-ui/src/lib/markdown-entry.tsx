e448f0a5834a07f7c8c85651ce2219337c6ced5a
Matt DeKrey <mattdekrey@gmail.com>
2022-01-24 19:42:25 -0600

    Add link to Table of Contents

--- game-powers-ui/src/lib/markdown-entry.tsx
+++ game-powers-ui/src/lib/markdown-entry.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from 'react';
+import { ChevronDoubleLeftIcon } from '@heroicons/react/solid';
 import { createEntry as originalCreateEntry } from 'lib/createEntry';
 import { ReaderLayout } from 'components/reader-layout';
 
@@ -6,6 +7,12 @@ export function createEntry(Component: FunctionComponent<any>) {
 	function App() {
 		return (
 			<ReaderLayout>
+				{window.location.pathname !== '/' ? (
+					<a href="/" className="underline text-theme float-right print:hidden">
+						<ChevronDoubleLeftIcon className="h-em w-em inline-block mr-2" />
+						Table of Contents
+					</a>
+				) : null}
 				<Component />
 			</ReaderLayout>
 		);
