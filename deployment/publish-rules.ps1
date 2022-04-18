6423110f8e2e9a59079d7fede0f1933f749a8559
Matt DeKrey <mattdekrey@gmail.com>
2022-04-17 21:42:23 -0500

    Correct deployment script

--- deployment/publish-rules.ps1
+++ deployment/publish-rules.ps1
@@ -1,22 +1,40 @@
+param (
+    [String]
+    $subscription = '2351fc7a-207c-4a7d-8104-d5fe21d7907f',
+
+    [String]
+    $repository = 'dekreydotnet',
+    [String]
+    $imageName = 'game-4e',
+
+    [String]
+    $azureResourceGroup = 'DeKreyDotNet',
+    [String]
+    $azureAksCluster = 'TinyKubed',
+
+    [String]
+    $k8sNamespace = 'game-engine',
+    [String]
+    $chartName = 'main'
+)
 
 Push-Location "$PSScriptRoot/.."
 
 $tag = (Get-Date).ToString('yyyy-MM-ddTHH_mm_ss')
+$fullImageName = "$($repository).azurecr.io/$($imageName)"
 
 docker build . -f .\deployment\Dockerfile -t dekreydotnet.azurecr.io/game-4e:$tag
 
-az account set --subscription 2351fc7a-207c-4a7d-8104-d5fe21d7907f
+az account set --subscription $($subscription)
 
 az acr login --name dekreydotnet
 docker push dekreydotnet.azurecr.io/game-4e:$tag
 
-$ns = 'game-engine'
-$name = 'main'
-$fullImageName = 'dekreydotnet.azurecr.io/game-4e'
 $sslClusterIssuer = 'letsencrypt'
 $domain = '4e.dekrey.net'
 
-helm upgrade --install -n $ns $name --create-namespace mdekrey/single-container `
+az aks get-credentials --resource-group $azureResourceGroup -n $azureAksCluster
+helm upgrade --install -n $k8sNamespace $chartName --create-namespace mdekrey/single-container `
     --set-string "image.repository=$($fullImageName)" `
     --set-string "image.tag=$tag" `
     --set-string "ingress.annotations.cert-manager\.io/cluster-issuer=$sslClusterIssuer" `
