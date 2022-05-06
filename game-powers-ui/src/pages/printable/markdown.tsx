8be64624cc80866a7101ab52dc20b680110a55d0
Matt DeKrey <mattdekrey@gmail.com>
2022-05-06 06:55:03 -0500

    Move page builders

--- game-powers-ui/src/pages/printable/markdown.tsx
+++ game-powers-ui/src/pages/printable/markdown.tsx
@@ -1,32 +1,29 @@
 import { useState } from 'react';
-import { MdxComponents } from 'components/layout/mdx-components';
 import { MdxEditor } from 'components/monaco/MdxEditor';
+import { ReaderLayout } from 'components/reader-layout';
 import { FlavorText } from 'components/reader-layout/FlavorText';
 import { Inset } from 'components/reader-layout/inset';
 import { MainHeader } from 'components/reader-layout/MainHeader';
 import { Sidebar } from 'components/sidebar';
 import { FullReferenceMdx } from 'components/mdx/FullReferenceMdx';
-import { PowerDetailsSelector } from '../power-details/power.selector';
-import { Sources } from '../sources';
-import styles from './power-grid.module.css';
+import { PowerDetailsSelector } from '../legacy/power-details/power.selector';
+import { Sources } from '../legacy/sources';
 
-export function PowerGrid() {
+export function Markdown() {
 	const [mdx, setMdx] = useState('');
 	return (
 		<>
 			<div className="h-screen print:hidden p-16">
 				<MdxEditor value={mdx} onChange={setMdx} />
 			</div>
-			<div className={styles.powerGrid}>
-				<MdxComponents>
-					<Sidebar.Display value={false}>
-						<FullReferenceMdx
-							components={{ Inset, Sources, PowerDetailsSelector, MainHeader, FlavorText }}
-							contents={mdx}
-						/>
-					</Sidebar.Display>
-				</MdxComponents>
-			</div>
+			<ReaderLayout>
+				<Sidebar.Display value={false}>
+					<FullReferenceMdx
+						components={{ Inset, Sources, PowerDetailsSelector, MainHeader, FlavorText }}
+						contents={mdx}
+					/>
+				</Sidebar.Display>
+			</ReaderLayout>
 		</>
 	);
 }
