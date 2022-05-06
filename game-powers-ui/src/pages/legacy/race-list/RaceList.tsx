0502220b66f4a20e2c9b17e96964a61ed3ecb0d4
Matt DeKrey <mattdekrey@gmail.com>
2022-05-06 07:36:24 -0500

    Add per-status-code component

--- game-powers-ui/src/pages/legacy/race-list/RaceList.tsx
+++ game-powers-ui/src/pages/legacy/race-list/RaceList.tsx
@@ -2,32 +2,31 @@ import { sortBy } from 'lodash/fp';
 import { map } from 'rxjs/operators';
 import { useApi } from 'core/hooks/useApi';
 import { useObservable } from 'core/hooks/useObservable';
-import { StructuredResponses } from 'api/operations/getLegacyRaces';
+import { StructuredResponses, Responses } from 'api/operations/getLegacyRaces';
 import { initial, Loadable, makeLoaded } from 'core/loadable/loadable';
 import { ReaderLayout } from 'components/reader-layout';
 import { LoadableComponent } from 'core/loadable/LoadableComponent';
 import { LegacyRuleSummary } from 'api/models/LegacyRuleSummary';
 import { MainHeader } from 'components/reader-layout/MainHeader';
+import { StandardResponse } from '@principlestudios/openapi-codegen-typescript';
+import { ReactNode } from 'react';
 import { wizardsSort } from '../wizards-sort';
 
 export function RaceList() {
 	const api = useApi();
-	const data = useObservable(
-		() =>
-			api.getLegacyRaces().pipe(map((response) => makeLoaded(sortBy<LegacyRuleSummary>(wizardsSort, response.data)))),
-		initial as Loadable<StructuredResponses[200]['application/json'], never>
-	);
+	const list = useObservable(() => api.getLegacyRaces(), null);
 
 	return (
 		<ReaderLayout>
-			<LoadableComponent
-				data={data}
-				errorComponent={() => <>Not Found</>}
-				loadedComponent={(loaded) => (
+			<PerStatusCode<Exclude<typeof list, null>>
+				response={list}
+				loading={<>Loading</>}
+				other={() => <>Unknown Response from Server</>}
+				_200={(loaded) => (
 					<>
 						<MainHeader>Race List</MainHeader>
 						<ul className="list-disc ml-6 theme-4e-list">
-							{loaded.map(({ wizardsId, name, flavorText }) => (
+							{sortBy<LegacyRuleSummary>(wizardsSort, loaded.data).map(({ wizardsId, name, flavorText }) => (
 								<li key={wizardsId} className="my-1">
 									<a href={`/legacy/rule/${wizardsId}`} className="underline text-theme">
 										{name}
@@ -38,8 +37,31 @@ export function RaceList() {
 						</ul>
 					</>
 				)}
-				loadingComponent={<>Loading</>}
 			/>
 		</ReaderLayout>
 	);
+}
+
+type MapResponsesToReact<T extends StandardResponse> = {
+	[K in T['statusCode'] as `_${K}`]: (
+		response: T extends StandardResponse<K, 'application/json', infer TResponse>
+			? StandardResponse<K, 'application/json', TResponse>
+			: never
+	) => JSX.Element;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function PerStatusCode<T extends StandardResponse>({
+	response,
+	loading,
+	other,
+	...statusCodes
+}: {
+	response: T | null;
+	loading: ReactNode;
+	other: (response: StandardResponse) => JSX.Element;
+} & MapResponsesToReact<T>) {
+	if (response === null) return <>{loading}</>;
+	const sc = statusCodes as unknown as Record<string, typeof other>;
+	return <>{(sc[`_${response.statusCode}`] ?? other)(response)}</>;
 }
