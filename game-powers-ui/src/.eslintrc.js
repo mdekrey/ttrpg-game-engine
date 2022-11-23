733d5eb63dfb172a531ea1e4270bfab3dffab8b4
Matt DeKrey <mattdekrey@gmail.com>
2022-11-23 07:29:53 -0600

    Correct lint settings

--- game-powers-ui/src/.eslintrc.js
+++ game-powers-ui/src/.eslintrc.js
@@ -2,35 +2,35 @@ const path = require('path');
 
 module.exports = {
 	extends: ['plugin:@nrwl/nx/react', '../.eslintrc.js'],
-	ignorePatterns: ['!**/*', '.eslintrc.js', '**/*.mdx', 'api/**/*', 'foundry-bridge/**/*'],
+	ignorePatterns: ['!**/*', '.eslintrc.js', '**/*.mdx', 'api/**/*', 'foundry-bridge/**/*', '**/*.stories.*'],
 	overrides: [
 		{
 			files: ['*.ts', '*.tsx', '*.js', '*.jsx'],
-			rules: {},
+			rules: {}
 		},
 		{
 			files: ['*.spec.*'],
 			parserOptions: {
 				project: './tsconfig.json',
-				tsconfigRootDir: __dirname,
+				tsconfigRootDir: __dirname
 			},
-			rules: {},
+			rules: {}
 		},
 		{
 			files: ['*.ts', '*.tsx'],
 			parserOptions: {
 				project: './tsconfig.json',
-				tsconfigRootDir: __dirname,
+				tsconfigRootDir: __dirname
 			},
 			rules: {
 				// something appears to be wrong with the import library
 				'import/no-unresolved': 0,
-				'import/named': 0,
-			},
+				'import/named': 0
+			}
 		},
 		{
 			files: ['*.js', '*.jsx'],
-			rules: {},
-		},
-	],
+			rules: {}
+		}
+	]
 };
